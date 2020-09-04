9f14ab229ece0098acbd055acb3d8e29c96dbb34
Geni W <williamsgen@urbandaleschools.com>
2020-09-04 10:13:56 -0500

    software engineer

--- 1.1_Github_VCS.py
+++ 1.1_Github_VCS.py
@@ -46,7 +46,7 @@ My favorite color is green
 I have a cat and a dog
 I'm in show choir, cheer, & band
 My favorite movie is The Titanic
-
+When I'm older I plan to be a software engineer
 ------------------Student 05------------------
 
 
